8c357e8c4f7f2e7a56fbe5b50745f275739db0e6
rizwanit42-hue <rizwanit42@gmail.com>
2026-05-04 07:13:10 +0500

    heroku setup

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template,request,redirect,url_for
 from flask_mysqldb import MySQL
 from config import config
+import os
+import pymysql
 app=Flask(__name__)
 app.config.from_object(config)
 mysql = MySQL(app)
@@ -11,14 +13,14 @@ def index():
     record = cur.fetchall()
     cur.close()
     return render_template('index.html', data=record)
-
+#new code
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method=="POST":
         name = request.form['name']
         email = request.form['email']
         ph = request.form['phone']
-        courses = request.form['courses']
+        courses = request.form[ 'courses']
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO students (name, email, phone, course) VALUES (%s, %s, %s, %s)",(name,email,ph,courses))
         mysql.connection.commit()
--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 class config:
-    MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'student_db'
-    MYSQL_PORT = 3307
+    app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+    app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+    app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
